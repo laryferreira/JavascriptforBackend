b4506d6c11ae9b2fc79a75ffe304638efab3ee4d
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-22 13:24:22 -0300

    Create type_boolean.js

---
+++ Tipos_Variaveis_Funções/type_boolean.js
@@ -0,0 +1,14 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+let cadastroAtivado = true;
+console.log(primeiroNumero === segundoNumero); // false
+const texto1 = 'ana';
+const texto2 = 'Ana';
+console.log(texto1 === texto2);
+
+
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
