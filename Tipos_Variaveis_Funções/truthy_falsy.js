06ce9c746cd8ea86a0223effaec784581b1f20bd
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-22 13:47:14 -0300

    Create truthy_falsy.js

---
+++ Tipos_Variaveis_Funções/truthy_falsy.js
@@ -0,0 +1,24 @@
+// boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy
+// 0 -> false
+// 1 -> true
+
+//console.log(0 == 1);
+//console.log(0 == false);
+//console.log(''== false);
+//console.log(1 == true);
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let string = 'curso';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull); // null = object
