a9c1af2889cb7e5102b6d897d74a0d62aa4a8491
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-22 13:11:08 -0300

    Create type_string.js

---
+++ Tipos_Variaveis_Funções/type_string.js
@@ -0,0 +1,21 @@
+const texto1 = "olá, mundo!";
+const texto2 = "paçoca";
+const senha = "senhaSuperString!562@";
+const stringnumeros = "245637";
+const citacao = 'Meu nome é ';
+const nome = "Laryssa"
+console.log(citacao + nome)
+console.log(citacao)
+console.log(stringnumeros)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha2 = "minhaSenha123"
+console.log(senha2.length) // 13 caracteres
+
+// template string ou template literal
