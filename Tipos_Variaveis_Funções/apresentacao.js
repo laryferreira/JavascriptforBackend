321218adfa248b49c09a26aa64f9e8cde29268ce
Laryssa Ferreira <90197932+laryferreira@users.noreply.github.com>
2023-05-24 20:04:37 -0300

    Create apresentacao.js

---
+++ Tipos_Variaveis_Funções/apresentacao.js
@@ -0,0 +1,7 @@
+const nome = "Lary";
+const idade = 2023-2004;
+const cidade = "Brasília";
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade}`
+
+console.log(apresentacao)
